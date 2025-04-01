4f78ebf20013a45242e3d0936ccfa3ead24eb6aa
Hunter <akulkaushal13579@gmail.com>
2025-04-01 18:37:20 +0530

    modified and updated Display Table

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ app = FastAPI()
 def home():
     return {"message":{"Welcome to Book Store"}}
 
-@app.post("/shelf")
+
+# 1: Display/record
+@app.post("/shelf/")
 def get_details():
+    '''
+    Display Feature, to display the books in the store  
+    param : none
+    '''
     conn = Get_db_connection()
     cursor = conn.cursor()
 
@@ -27,4 +33,22 @@ def get_details():
     finally:
         cursor.close()
         conn.close()
-    
+
+
+
+class cart_item(BaseModel):
+    item: str
+
+@app.post("/cart/")
+def add_to_cart(object: cart_item):
+    conn = Get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        return {"message":"Added item to cart"}
+    except Exception as e:
+        raise HTTPException(status_code=221,detail=str(e))
+    finally:
+        cursor.close()
+        conn.close()
+
