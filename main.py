f045046c63bb5b34cbf4ae40525f572f53a020e9
Hunter <akulkaushal13579@gmail.com>
2025-04-02 23:44:25 +0530

    completed the display functionality

--- main.py
+++ main.py
@@ -16,27 +16,41 @@ def home():
 class filter(BaseModel):
     genre: Optional[str] = None,
     status: Optional[str] = None,
-    limit: int = 10,
-    offset: int = 0,
-
-
+    page_size: int = 10,
+    page: int = 0,
 @app.post("/shelf/")
-async def get_details():
+async def get_details(object: filter):
     '''
     Display Feature, to display the books in the store  
     param : none
     '''
     conn = Get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
 
     try:
-        query = f"select * from shelf"
-        cursor.execute(query)
+        query = f"select * from shelf where 1=1"
+        offset = (object.page-1)*object.page_size
+        params = []
+        if object.genre is None:
+            query += ""
+        else:
+            query += " and genre = %s"
+            params.append(object.genre)
+        if object.status is None:
+            query += ""
+        else:
+            query += " and status = %s"
+            params.append(object.status)
+        query += " limit %s offset %s"
+        params.extend([object.page_size,offset])
+
+        cursor.execute(query,tuple(params))
         # conn.commit()
         response = cursor.fetchall()
         return {"response":response}
+
     except Exception as e:
-        raise HTTPException(status_code=200,detail=str(e))
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
     finally:
         cursor.close()
         conn.close()
@@ -51,7 +65,9 @@ def add_to_cart(object: cart_item):
     conn = Get_db_connection()
     cursor = conn.cursor()
 
+    
     try:
+        cursor.execute("insert into cart (book_id, product, price, quantity) values (1, 'Harry Potter', 19.99, 1) on duplicate key update quantity = quantity + 1;")
         return {"message":"Added item to cart"}
     except Exception as e:
         raise HTTPException(status_code=221,detail=str(e))
